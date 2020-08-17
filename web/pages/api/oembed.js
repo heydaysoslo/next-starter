37732121b68bf285e551439b8f9779017fdd6784
mittnavnermike <niiimon@gmail.com>
2020-08-17 17:14:54 +0200

    updated Oembed

--- web/pages/api/oembed.js
+++ web/pages/api/oembed.js
@@ -1,29 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { extract } = require('oembed-parser')
 
 export default (req, res) => {
-  console.log('res', res)
-  console.log('req', req)
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-  // const { url } = JSON.parse(event.body)
-
-  // extract(url)
-  //   .then(data => {
-  //     callback(null, {
-  //       statusCode: 200,
-  //       body: JSON.stringify({
-  //         result: data,
-  //         status: 'success'
-  //       })
-  //     })
-  //   })
-  //   .catch(err => {
-  //     callback(null, {
-  //       statusCode: 500,
-  //       body: JSON.stringify({
-  //         error: err,
-  //         status: 'error'
-  //       })
-  //     })
-  //   })
+  const { url } = JSON.parse(req.body)
+  console.log('url', url)
+  extract(url)
+    .then(data => {
+      res.statusCode = 200
+      res.json({
+        statusCode: 200,
+        result: data,
+        status: 'success'
+      })
+    })
+    .catch(err => {
+      res.statusCode = 500
+      res.json({
+        statusCode: 500,
+        error: err,
+        status: 'error'
+      })
+    })
 }
