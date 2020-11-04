110081b48c5c298708f7c5fece1982d5e7e72b7c
mittnavnermike <niiimon@gmail.com>
2020-11-04 13:52:50 +0100

    :rocket: import() lazyloading of components

--- web/components/pagebuilder/Pagebuilder.js
+++ web/components/pagebuilder/Pagebuilder.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import dynamic from 'next/dynamic'
+import React, { useEffect, useState } from 'react'
 import styled, { css } from 'styled-components'
 
 import Stagger from '@heydays/animation/Stagger'
 
 const sectionTypes = {
-  section: dynamic(() => import('./Section')),
-  cardSection: dynamic(() => import('./CardSection')),
-  textSection: dynamic(() => import('./TextSection')),
-  imageSection: dynamic(() => import('./FullImageSection')),
-  textImageSplit: dynamic(() => import('./TextImageSplit')),
-  carousel: dynamic(() => import('./CarouselSection')),
-  tabs: dynamic(() => import('../elements/Tabs')),
-  videoSection: dynamic(() => import('./VideoSection')),
+  section: 'pagebuilder/Section.js',
+  cardSection: 'pagebuilder/CardSection.js',
+  textSection: 'pagebuilder/TextSection.js',
+  imageSection: 'pagebuilder/FullImageSection.js',
+  textImageSplit: 'pagebuilder/TextImageSplit.js',
+  carousel: 'pagebuilder/TextImageSplit.js',
+  tabs: 'elements/Tabs.js',
+  videoSection: 'pagebuilder/VideoSection',
+  menu: 'Menu.js',
+  scrollAnchor: 'pagebuilder/ScrollAnchor',
 }
 
 const StyledPageBuilder = styled.div(
@@ -27,24 +28,38 @@ const PageBuilder = ({ sections }) => {
   return (
     <StyledPageBuilder>
       <Stagger>
-        {sections?.map((section, index) => {
-          const Component = sectionTypes[section._type] || null
-          return Component ? (
-            <div key={section._key} className="PageBuilder__item">
-              <Component
-                {...section}
-                prevComp={sections[index - 1] ? sections[index - 1] : null}
-                nextComp={sections[index + 1] ? sections[index + 1] : null}
-              />
-            </div>
-          ) : (
-            <p key={section._key} style={{ background: 'yellow' }}>
-              Component {section._type} not found
-            </p>
-          )
-        })}
+        {sections?.map((section, index) => (
+          <PageBuilderResolver
+            key={section._key}
+            section={section}
+            {...section}
+            prevComp={sections[index - 1] ? sections[index - 1] : null}
+            nextComp={sections[index + 1] ? sections[index + 1] : null}
+          />
+        ))}
       </Stagger>
     </StyledPageBuilder>
+  )
+}
+
+const PageBuilderResolver = ({ section, ...props }) => {
+  const [Component, setComponent] = useState(null)
+
+  useEffect(() => {
+    if (sectionTypes[section._type]) {
+      // Dynamically import template
+      import(`../../components/${sectionTypes[section._type]}`)
+        .then((comp) => setComponent(() => comp.default))
+        .catch((err) => console.log(err))
+    }
+  }, [section])
+
+  return Component ? (
+    <Component className="PageBuilder__item" section={section} {...props} />
+  ) : (
+    <p key={section._key} style={{ background: 'red' }}>
+      Component {section._type} not found
+    </p>
   )
 }
 
