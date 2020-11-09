41ca2c88f47894afef64db16014b6ae767bb19fc
mittnavnermike <niiimon@gmail.com>
2020-11-09 16:33:55 +0100

    ✨ Added global content + :recycle: deskstructure

--- cms/schemas/documents/companyInfo.js
+++ cms/schemas/documents/companyInfo.js
@@ -1,85 +1,84 @@
-import MdBusiness from "react-icons/lib/md/business";
+import MdBusiness from 'react-icons/lib/md/business'
 
 export default {
-  name: "companyInfo",
-  title: "Company Info",
-  type: "document",
+  name: 'companyInfo',
+  title: 'Company Info',
+  type: 'document',
   liveEdit: false,
-  __experimental_actions: ["update", "publish" /*'create', 'delete'*/],
   icon: MdBusiness,
   fieldsets: [
-    { name: "offices", title: "Offices" },
-    { name: "contact", title: "Contact" },
+    { name: 'offices', title: 'Offices' },
+    { name: 'contact', title: 'Contact' },
     {
-      name: "location",
-      title: "Location",
+      name: 'location',
+      title: 'Location',
       description:
-        "These fields are used to tell google where the business is located."
+        'These fields are used to tell google where the business is located.'
     }
   ],
   fields: [
     {
-      name: "name",
-      title: "Company name",
-      type: "string",
-      fieldset: "contact"
+      name: 'name',
+      title: 'Company name',
+      type: 'string',
+      fieldset: 'contact'
     },
     {
-      name: "email",
-      title: "Email",
-      type: "email",
-      fieldset: "contact"
+      name: 'email',
+      title: 'Email',
+      type: 'email',
+      fieldset: 'contact'
     },
     {
-      name: "phone",
-      title: "Phone number",
-      type: "string",
-      fieldset: "contact"
+      name: 'phone',
+      title: 'Phone number',
+      type: 'string',
+      fieldset: 'contact'
     },
     {
-      name: "address",
-      title: "Address",
-      type: "address",
-      fieldset: "location"
+      name: 'address',
+      title: 'Address',
+      type: 'address',
+      fieldset: 'location'
     },
     {
-      name: "lat",
-      title: "Latitude",
+      name: 'lat',
+      title: 'Latitude',
       description:
-        "Follow a pattern similar to: 59.926919. You can find the coordinates if you go to https://maps.google.com",
-      type: "string",
-      fieldset: "location"
+        'Follow a pattern similar to: 59.926919. You can find the coordinates if you go to https://maps.google.com',
+      type: 'string',
+      fieldset: 'location'
     },
     {
-      name: "lng",
-      title: "Longitude",
+      name: 'lng',
+      title: 'Longitude',
       description:
-        "Follow a pattern similar to: 59.926919. You can find the coordinates if you go to https://maps.google.com",
-      type: "string",
-      fieldset: "location"
+        'Follow a pattern similar to: 59.926919. You can find the coordinates if you go to https://maps.google.com',
+      type: 'string',
+      fieldset: 'location'
     },
     {
-      name: "orgNumber",
-      title: "Organization number",
-      type: "string",
-      fieldset: "contact"
+      name: 'orgNumber',
+      title: 'Organization number',
+      type: 'string',
+      fieldset: 'contact'
     },
     {
-      name: "social",
-      title: "Social",
-      type: "social"
+      name: 'social',
+      title: 'Social',
+      type: 'social'
     },
     {
-      name: "offices",
-      title: "Offices",
-      type: "array",
+      name: 'offices',
+      title: 'Offices',
+      type: 'array',
       of: [
         {
-          name: "office",
-          title: "Office",
-          type: "office"
+          name: 'office',
+          title: 'Office',
+          type: 'office'
         }
       ]
     }
   ]
-};
+}
