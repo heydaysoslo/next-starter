749f3c262d8f7671e95e0f55095a16e2618aad56
Martin Sanne Kristiansen <martin@heydays.no>
2020-11-10 13:17:23 +0100

    💄 contained admin bar

--- web/components/cms/AdminBar.js
+++ web/components/cms/AdminBar.js
@@ -2,21 +2,26 @@ import React from 'react'
 import Link from 'next/link'
 import { useCurrentUser } from '@cms'
 import styled from 'styled-components'
+import Container from '@heydays/Container'
 
 const AdminBar = () => {
   const user = useCurrentUser()
   return (
     <StyledAdminBar>
-      <strong>Preview mode</strong>
-      <span>The content you see might not reflect the published website.</span>
-      {user && (
+      <Container>
+        <strong>Preview mode</strong>
         <span>
-          Logged in as <strong>{user?.data?.name}</strong>
+          The content you see might not reflect the published website.
         </span>
-      )}
-      <Link href="/api/exit-preview">
-        <a>Leave preview mode</a>
-      </Link>
+        {user && (
+          <span>
+            Logged in as <strong>{user?.data?.name}</strong>
+          </span>
+        )}
+        <Link href="/api/exit-preview">
+          <a>Leave preview mode</a>
+        </Link>
+      </Container>
     </StyledAdminBar>
   )
 }
