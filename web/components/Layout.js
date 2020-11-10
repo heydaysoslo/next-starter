4bbea8014a5c301c214a8b47592b554f2f1bda22
Martin Sanne Kristiansen <martin@heydays.no>
2020-11-10 12:24:43 +0100

    sticky footer

--- web/components/Layout.js
+++ web/components/Layout.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
+import Header from '../components/Header'
 import SEO from '../components/SEO'
+import Footer from '@heydays/Footer'
+import styled from 'styled-components'
 
 const AdminBar = dynamic(() => import('../components/cms/AdminBar'), {
   ssr: false
@@ -8,12 +11,30 @@ const AdminBar = dynamic(() => import('../components/cms/AdminBar'), {
 
 const Layout = ({ page, preview = false, children }) => {
   return (
-    <>
+    <Wrapper>
       <SEO page={page} />
-      {preview && <AdminBar />}
-      {children}
-    </>
+      <Content>
+        {preview && <AdminBar />}
+        <Header />
+        {children}
+      </Content>
+      <StyledFooter />
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  min-height: 100%;
+`
+
+const Content = styled.div`
+  flex: 1 0 auto;
+`
+const StyledFooter = styled(Footer)`
+  flex-shrink: 0;
+`
 
 export default Layout
