ae4dbc4f1362ff2e47f2a388e86c0331e1726a4a
Martin Sanne Kristiansen <martin@heydays.no>
2020-11-10 18:24:02 +0100

    🎨 remove test components

--- web/components/pages/FrontPage.js
+++ web/components/pages/FrontPage.js
@@ -2,19 +2,14 @@ import React from 'react'
 import styled, { css } from 'styled-components'
 
 import Pagebuilder from '../pagebuilder/Pagebuilder'
-import { H1, P } from '@heydays/Typography'
+import { H1 } from '@heydays/Typography'
 import Container from '@heydays/Container'
-import Button from '@heydays/Button'
 
 const FrontPage = ({ className, title, pagebuilder }) => {
   return (
     <div className={className}>
       <Container className="Page__container">
-        <Button modifiers="active">Button</Button>
-        <header className="Page__header">
-          <P>FrontPage</P>
-          {title && <H1>{title}</H1>}
-        </header>
+        <header className="Page__header">{title && <H1>{title}</H1>}</header>
         {pagebuilder && (
           <div className="Page__content">
             {pagebuilder?.sections && (
