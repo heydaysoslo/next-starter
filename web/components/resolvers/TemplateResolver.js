f19b5799ad2e1d7d06b5f0b62c4e0c928761cdf4
Martin Sanne Kristiansen <martin@heydays.no>
2020-11-12 11:57:33 +0100

    🎨 make sure templates are ssr

--- web/components/resolvers/TemplateResolver.js
+++ web/components/resolvers/TemplateResolver.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import dynamic from 'next/dynamic'
 
 const templates = {
-  frontpage: 'FrontPage',
-  contact: 'ContactPage',
-  news: 'NewsPage',
-  default: 'Page',
-  article: 'Article'
+  frontpage: dynamic(() => import('../pages/FrontPage')),
+  contact: dynamic(() => import('../pages/ContactPage')),
+  news: dynamic(() => import('../pages/NewsPage')),
+  default: dynamic(() => import('../pages/Page'))
+}
+
+const types = {
+  article: dynamic(() => import('../pages/Article'))
 }
 
 export default function TemplateResolver({ page }) {
-  const [Component, setComponent] = useState(null)
+  let Component = null
 
-  useEffect(() => {
-    let temp
-    // Check if we have a template
-    if (page?.template && templates[page.template]) {
-      temp = templates[page.template]
-    } // If no template name is set, resolve to type
-    else if (page?._type && templates[page._type]) {
-      temp = templates[page._type]
-    } // If we still don't have a component, resolve to default
-    else {
-      temp = templates.default
+  // Check if we a page template is defined
+  if (templates[page?.template]) {
+    Component = templates[page.template]
+  }
+
+  // If no template name is set, resolve to type
+  if (!Component) {
+    if (page?._type && types[page._type]) {
+      Component = types[page._type]
     }
+  }
 
-    // Dynamically import template
-    import(`../pages/${temp}`)
-      .then(comp => setComponent(() => comp.default))
-      .catch(err => console.log(err))
-  }, [page])
-
-  if (!Component) return <div style={{ minHeight: '100vh' }} />
+  // If we still don't have a component, resolve to default
+  if (!Component) {
+    Component = templates.default
+  }
 
   return <Component {...page} />
 }
