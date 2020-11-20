f484e0132d6bcb4d46884b7160cc2cb1572c487c
Martin Sanne Kristiansen <martin@heydays.no>
2020-11-20 16:16:39 +0100

    page template with hero

--- web/components/pagebuilder/HeroSection.js
+++ web/components/pagebuilder/HeroSection.js
@@ -3,13 +3,15 @@ import Spacer from '@heydays/Spacer'
 import Container from '@heydays/Container'
 import { H1 } from '@heydays/Typography'
 import styled, { css } from 'styled-components'
+import Editor from 'components/editor'
 
-const HeroSection = ({ className, title }) => {
+const HeroSection = ({ className, title, intro }) => {
   return (
     <header className={className}>
       <Container>
         <Spacer size="header" />
         <H1>{title}</H1>
+        {intro && <Editor blocks={intro} />}
       </Container>
     </header>
   )
--- web/components/pages/Page.js
+++ web/components/pages/Page.js
@@ -1,25 +1,30 @@
+import HeroSection from '@heydays/HeroSection'
 import React from 'react'
 import styled, { css } from 'styled-components'
 
-// import Pagebuilder from '../pagebuilder/Pagebuilder'
-import PageBuilderNew from '../pagebuilder/PageBuilderNew'
-import { H1 } from '@heydays/Typography'
-import Container from '@heydays/Container'
+import Pagebuilder from '../pagebuilder/Pagebuilder'
+// import Pagebuilder from '../pagebuilder/PageBuilderNew'
+// import { H1 } from '@heydays/Typography'
+// import Container from '@heydays/Container'
 
-const Page = ({ className, title, content, pagebuilder, ...props }) => {
+const Page = ({
+  className,
+  title,
+  content,
+  pagebuilder,
+  excerpt,
+  ...props
+}) => {
   return (
-    <div className={className}>
-      <header>
-        <Container>{title && <H1>{title}</H1>}</Container>
-      </header>
-      {pagebuilder && (
-        <div className="Page__content">
-          {pagebuilder?.sections && (
-            <PageBuilderNew sections={pagebuilder.sections} />
-          )}
-        </div>
+    <article className={className}>
+      {/**
+       * Use a hero section with the page content if no custom hero is added to the pagebuilder
+       */}
+      {pagebuilder?.sections[0]?._type !== 'heroSection' && (
+        <HeroSection title={title} intro={excerpt} />
       )}
-    </div>
+      {pagebuilder?.sections && <Pagebuilder sections={pagebuilder.sections} />}
+    </article>
   )
 }
 
