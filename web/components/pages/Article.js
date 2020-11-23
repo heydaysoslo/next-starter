781c3c3b40f2fc5e3bdac7a9065698c74100e364
Martin Sanne Kristiansen <martin@heydays.no>
2020-11-23 11:11:30 +0100

    cleanup article

--- web/components/pages/Article.js
+++ web/components/pages/Article.js
@@ -1,45 +1,36 @@
 import React from 'react'
 
+import { H1 } from '@heydays/Typography'
+import Container from '@heydays/Container'
+import Spacer from '@heydays/Spacer'
 import { getReadTime } from '../../utils/sanityHelpers'
 import Editor from '../editor'
-import Grid, { GridItem } from '@heydays/Grid'
-import Container from '@heydays/Container'
 import PageBuilder from '../pagebuilder/Pagebuilder'
 import Author from '../elements/Author'
 
 const Article = page => {
-  const { title, body, _rawMainImage, authors, publishDate, pagebuilder } = page
+  const { title, body, authors, publishDate, pagebuilder } = page
   return (
-    <Container>
-      <article className="Article">
-        <Grid reverse={{ md: true }}>
-          <GridItem span={{ sm: 12, md: 9 }}>
-            <header>
-              {title && <h1>{title}</h1>}
-              {_rawMainImage && (
-                <div className="Article__image">
-                  <SanityImage node={_rawMainImage} />
-                </div>
-              )}
-            </header>
-            {body && (
-              <div className="Article__content">
-                <Editor blocks={body} />
-              </div>
-            )}
-          </GridItem>
-          <GridItem span={{ md: 3 }}>
+    <article>
+      <Spacer size="header" />
+      <header>
+        <Container>
+          {title && <H1>{title}</H1>}
+          <p>
             {body && `Read time: ${getReadTime(body)}min`}
-            {publishDate && <p className="Article__date">{publishDate}</p>}
-            {authors &&
-              authors.map(author => <Author key={author._key} {...author} />)}
-          </GridItem>
-        </Grid>
-        {pagebuilder?.sections && (
-          <PageBuilder sections={pagebuilder.sections} />
-        )}
-      </article>
-    </Container>
+            {publishDate && <span>Published: {publishDate}</span>}
+          </p>
+        </Container>
+      </header>
+      {body && (
+        <Container>
+          <Editor blocks={body} />
+        </Container>
+      )}
+      {authors &&
+        authors.map(author => <Author key={author._key} {...author} />)}
+      {pagebuilder?.sections && <PageBuilder sections={pagebuilder.sections} />}
+    </article>
   )
 }
 
