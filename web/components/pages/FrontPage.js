e342c57ad00c8f3a81e92d9d2a30feff19083456
Martin Sanne Kristiansen <martin@heydays.no>
2020-11-23 12:38:42 +0100

    fix frontpage template

--- web/components/pages/FrontPage.js
+++ web/components/pages/FrontPage.js
@@ -1,34 +1,16 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
-
 import Pagebuilder from '../pagebuilder/Pagebuilder'
-import { H1 } from '@heydays/Typography'
-import Container from '@heydays/Container'
+import HeroSection from '@heydays/HeroSection'
 
 const FrontPage = ({ className, title, pagebuilder }) => {
   return (
-    <div className={className}>
-      <Container className="Page__container">
-        <header className="Page__header">{title && <H1>{title}</H1>}</header>
-        {pagebuilder && (
-          <div className="Page__content">
-            {pagebuilder?.sections && (
-              <Pagebuilder sections={pagebuilder.sections} />
-            )}
-          </div>
-        )}
-      </Container>
-    </div>
+    <article className={className}>
+      {pagebuilder?.sections[0]?._type !== 'heroSection' && (
+        <HeroSection title={title} />
+      )}
+      {pagebuilder?.sections && <Pagebuilder sections={pagebuilder.sections} />}
+    </article>
   )
 }
 
-export default styled(FrontPage)(
-  ({ theme }) => css`
-    .Page__header {
-      ${theme.spacing.sm('mt')}
-    }
-    .Page__content {
-      ${theme.spacing.sm('mt')}
-    }
-  `
-)
+export default FrontPage
