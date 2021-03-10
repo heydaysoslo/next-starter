d9017116a88d798f6194e463b2544b32d8b270d4
mittnavnermike <niiimon@gmail.com>
2021-03-10 14:37:08 +0100

    ♻️ rewrite useSanity to ts

--- web/components/hooks/useSanity.ts
+++ web/components/hooks/useSanity.ts
@@ -1,6 +1,13 @@
 import { useContext } from 'react'
 import SanityContext from '../context/sanityContext'
 
+/**
+ * Get sanity data from context
+ *
+ * @example ```tsx
+ *  const sanity = useSanity()
+ * ```
+ */
 const useSanity = () => {
   const sanity = useContext(SanityContext)
   return sanity
